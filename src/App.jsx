725191e5d8e4dba0acd10cd4c469e33588a90541
mintesnot <mintesnottesfaye@gmail.com>
2024-09-09 16:17:49 +0300

    axios request and envadded

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/home/Home'
+// import Header from './Components/Header/Header'
+
+function App() {
+
+
+  return (
+<div className='App'>
+      <Home/>
+     
+</div>
+   
+  )
+}
+
+export default App
